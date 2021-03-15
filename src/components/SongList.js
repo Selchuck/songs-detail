3218f3110b80dfda8998f8888e4028723e2dd880
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-03-15 15:39:56 +0000

    add mapStateToProps function to SongList component, pass as first argument to connect funtion

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,12 +3,17 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        SongList
+        {this.props.songs[0].title}
       </div>
     );
   };
 };
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+ return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
