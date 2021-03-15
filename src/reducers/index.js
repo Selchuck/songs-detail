2faccbabdd81a667f80af5b68554fd7de15423ac
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-03-15 13:25:35 +0000

    combine the two reducer functions using combineReducers method  and export

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
-export const songsReducer = () => {
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
   return [
     { 
       artist: 'Queen',  
@@ -88,3 +90,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
